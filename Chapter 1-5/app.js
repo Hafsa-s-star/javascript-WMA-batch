c112b08e64d07f12f04ff5bbedf7fdbb63e50652
Hafsa-s-star <pakistan.hafsa@gmail.com>
2025-07-31 18:02:20 +0500

    not completed

---
+++ Chapter 1-5/app.js
@@ -0,0 +1,56 @@
+// ---------Assignment 1 (Alerts) ---------
+// Question 1
+alert("Welcome To JS learning")
+
+// Question 2
+alert("Error! please enter a valid password.")
+
+// Question 3
+alert("Welcome to JS Land... \n Happy Coding!")
+
+// Question 4
+alert("Welcome to JS Land...")
+
+// Question 5 is in Console
+
+// Question 6 is in HTML file
+
+
+// ---------Assignment 2 (Variables for Strings)---------
+// Ouestion 1
+var userName = 
+alert(userName);
+
+// Question 2
+var myName = "Hafsa Bilal"
+alert(myName);
+
+// Question 3
+var message = "Hello World";
+alert(message);
+
+// Question 4
+var studentName = "Hafsa";
+var age = "13 year old";
+var course = "Mobile Web and App Development"
+
+// Question 5
+var food = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(food);
+
+// Question 6
+var email = "My Email Address is ";
+var emailAddress = "pakistan.hafsa@gmail.com";
+alert(email + emailAddress)
+
+// Question 7
+var bookDescription = "I am trying to learn form the Book  "
+var book = "A Smarter Way to Learn Javascript";
+alert(bookDescription + book);
+
+// Question 8
+
+
+// Question 9
+var end = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(end);
